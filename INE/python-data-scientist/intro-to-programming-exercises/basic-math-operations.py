f979f13f51229df7ce19a7d95e4c7c3b1c75af32
Tw3lly <94544321+Tw3lly@users.noreply.github.com>
2022-04-29 11:59:29 +0200

    Create basic-math-operations.py

---
+++ INE/python-data-scientist/intro-to-programming-exercises/basic-math-operations.py
@@ -0,0 +1,11 @@
+one_plus_one = 1 + 1
+ten_minus_three = 10 - 3
+four_times_five = 4 * 5
+nine_divided_by_three = 9 / 3
+
+# Modulus
+# This gets the remainder from a division problem
+five_mod_two = 5 % 2
+
+# Exponent
+three_to_fifth_power = 3**5
